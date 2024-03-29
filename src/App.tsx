7fbdadba2e5df117777e3fed1de180952073dac8
bnd-ev0530 <154604304+bnd-ev0530@users.noreply.github.com>
2024-03-29 14:58:48 +0900

    #3.1 DefinitelyTyped (09:51)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+const animation = keyframes`
+0%{
+  transform: rotate(0deg);
+  border-radius: 0px;
+}
+50%{
+  transform: rotate(360deg);
+  border-radius: 100px;
+}
+100%{
+  transform: rotate(0deg);
+  border-radius: 0px;
+}
+`;
+const Emoji = styled.span`
+  font-size: 30px;
+  transition: all 0.2s linear;
+  &:active {
+    opacity: 0;
+  }
+`;
+const Box = styled.div`
+  height: 100px;
+  width: 100px;
+  background-color: tomato;
+  // animation: ${animation} 1/s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 0.2s linear;
+  ${Emoji} {
+    &:hover {
+      font-size: 60px;
+    }
+  }
+`;
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>😁</Emoji>
+      </Box>
+      <Emoji>😁</Emoji>
+    </Wrapper>
+  );
 }
 
 export default App;
