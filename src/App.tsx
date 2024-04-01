d8b57a4452b377c8d975eff73748af030ab9963a
bnd-ev0530 <154604304+bnd-ev0530@users.noreply.github.com>
2024-04-01 14:23:43 +0900

    #3.6 Themes (08:42)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import React, { useState } from "react";
 import styled, { keyframes } from "styled-components";
-import Circle from "./Circle";
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = e;
+    setValue(value);
+  };
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Hello", value);
+  };
   return (
-    <>
-      <Circle bgColor="tomato"></Circle>
-      <Circle bgColor="teal" borderColor="red" text="dfdf"></Circle>
-    </>
+    <Container>
+      <H1>Hello!</H1>
+    </Container>
   );
 }
 
